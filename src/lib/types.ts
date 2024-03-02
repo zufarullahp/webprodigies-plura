e962109b0da6df1cd8155370db3018108e8b71f4
zufarullahp <a.zufarullahp@gmail.com>
2024-03-02 21:12:24 +0700

    basic setup pipeline

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,7 +1,8 @@
-import { Notification, Prisma, Role } from "@prisma/client";
-import { getAuthUserDetails, getMedia, getUserPermissions } from "./queries";
+import { Contact, Lane, Notification, Prisma, Role, Tag, Ticket, User } from "@prisma/client";
+import { _getTicketsWithAllRelations, getAuthUserDetails, getMedia, getPipelineDetails, getTicketsWithTags, getUserPermissions } from "./queries";
 import { db } from "./db";
 import { PrimitiveLabelProps } from "@radix-ui/react-label";
+import { z } from "zod";
 
 export type NotificationWithUser =
   | ({
@@ -47,3 +48,45 @@ Prisma.PromiseReturnType<
 export type GetMediaFiles = Prisma.PromiseReturnType<typeof getMedia>
 
 export type createMediaType = Prisma.MediaCreateWithoutSubaccountInput
+
+export type TicketAndTags = Ticket & {
+  Tags: Tag[];
+  Assigned : User | null;
+  Customer : Contact | null;
+} 
+
+export type LaneDetail = Lane & {
+  Tickets : TicketAndTags[]
+}
+
+export const CreatePipelineFormSchema = z.object({
+  name : z.string().min(1)
+})
+
+export const CreateFunnelFormSchema = z.object({
+  name: z.string().min(1),
+  description: z.string(),
+  subDomainName: z.string().optional(),
+  favicon: z.string().optional(),
+})
+
+export type PipelineDetailsWithLanesCardsTagsTickets = Prisma.PromiseReturnType<
+typeof getPipelineDetails>
+
+export const LaneFormSchema = z.object({
+  name : z.string().min(1)
+})
+
+export type TicketWithTags = Prisma.PromiseReturnType<typeof getTicketsWithTags>
+
+export type TicketDetails = Prisma.PromiseReturnType<typeof _getTicketsWithAllRelations>
+
+const currencyNumberRegex = /^\d+(\.\d{1,2})?$/
+
+export const TicketFormSchema = z.object({
+  name: z.string().min(1),
+  description: z.string().optional(),
+  value: z.string().refine((value) => currencyNumberRegex.test(value), {
+    message: 'Value must be a valid price.',
+  }),
+})
